c5eb824fa7d9cfc6198076cc93b002ede570726a
resumespeed <chengsaeteurn1985@gmail.com>
2022-04-01 15:31:56 -0700

    add comments functions

--- js/main.js
+++ js/main.js
@@ -12,3 +12,23 @@ const winningConditions = [
     ['topLeft','midCenter','bottomRight'],
     ['topRight','midCenter','bottomLeft']
 ];
+const squares = document.querySelectorAll('td');
+squares.onClick = function(e){
+    e.target.style.backgroundColor='red';
+}
+
+// document.querySelector('table').addEventListener('click', function (e){
+//     const topL = document.getElementById('topLeft');
+//     const topC = document.getElementById('topCenter');
+//     const topR = document.getElementById('topRight');
+//     //const turnRed = document.querySelectorAll('td div');
+//     //squares.style.backgroundColor = 'red';
+    
+//     topL.style.backgroundColor = 'red';
+//     topC.style.backgroundColor = 'red';
+//     topR.style.backgroundColor = 'red';
+// });
+
+
+//logic to win
+//if(winningConditions)
