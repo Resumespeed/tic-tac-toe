fae47e3e948148480d81225ca0dd54c4ef0af623
resumespeed <chengsaeteurn1985@gmail.com>
2022-04-03 15:08:11 -0700

    add remaining moves, comment out getWinner function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let player1=[];
 let player2=[];
+let remainingMoves = 9;
 
 let boardArray=[null,null,null,
     null,null,null,
@@ -27,7 +28,9 @@ squares.forEach(square => {
         boardArray.splice(square.id);
         console.log(boardArray);
         playerTurn = false;
-        console.log(getWinner());
+        // console.log(getWinner());
+        remainingMoves--;
+        console.log("remaining moves "+remainingMoves);
       }
     else{
         square.innerText = "0";
@@ -36,14 +39,16 @@ squares.forEach(square => {
         boardArray.push(square.id);
         console.log(boardArray);
         playerTurn = true;
+        remainingMoves--;
+        console.log(`remaining moves ${remainingMoves}`);
       }
     })
 });
 
-function getWinner(){
-  for(let i = 0; i < winningConditions.length; i++){
-    if((player1.winningConditions[i][0] + player1.winningConditions[i][1]+player1.winningConditions[i][2])===3){
-      return player1.winningConditions[i][0];
-    }
-  }
-}
+// function getWinner(){
+//   for(let i = 0; i < winningConditions.length; i++){
+//     if((player1[i][0] + player1[i][1]+player1[i][2])===3){
+//       return player1[i][0];
+//     }
+//   }
+// }
