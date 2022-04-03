7a6c8d850c9121dca937bd324e5e9d547af4e390
resumespeed <chengsaeteurn1985@gmail.com>
2022-04-03 14:55:28 -0700

    add win conditions but not working

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-let player1, player2
+let player1=[];
+let player2=[];
+
 let boardArray=[null,null,null,
     null,null,null,
     null,null,null];
@@ -14,17 +16,34 @@ const winningConditions = [
     ['topRight','midCenter','bottomLeft']
 ];
 const squares = document.querySelectorAll('td');
-
+// add once:true on 21 @ 27mins
 squares.forEach(square => {
   square.addEventListener('click', function handleClick(e) {
     console.log('square clicked', e);
     if(playerTurn){
         square.innerText="X";
+        player1.push(square.id);
+        console.log(player1);
+        boardArray.splice(square.id);
+        console.log(boardArray);
         playerTurn = false;
-    }
+        console.log(getWinner());
+      }
     else{
         square.innerText = "0";
+        player2.push(square.id);
+        console.log(player2);
+        boardArray.push(square.id);
+        console.log(boardArray);
         playerTurn = true;
-    }
-  })
+      }
+    })
 });
+
+function getWinner(){
+  for(let i = 0; i < winningConditions.length; i++){
+    if((player1.winningConditions[i][0] + player1.winningConditions[i][1]+player1.winningConditions[i][2])===3){
+      return player1.winningConditions[i][0];
+    }
+  }
+}
